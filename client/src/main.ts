88a7208bc87f1da285c83fdbeb5891fce5bb5474
ikwemogena <abdulaimogena@gmail.com>
2025-05-12 13:14:50 +0100

    Add global debounce directive to Vue app
    Integrated a custom 'debounce' directive into the app using `useDirectives`. This enhances the app by providing a reusable, globally available debounce functionality for event bindings.

--- client/src/main.ts
+++ client/src/main.ts
@@ -3,5 +3,8 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from "./router";
+import {useDirectives} from "./composables/useDirectives.ts";
 
-createApp(App).use(router).mount('#app')
+const {vDebounce} = useDirectives()
+
+createApp(App).use(router).directive('debounce', vDebounce).mount('#app')
