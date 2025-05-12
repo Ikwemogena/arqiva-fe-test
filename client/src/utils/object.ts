5201ad5e1981ee602b5d1bbac9d45f21575e17ad
ikwemogena <abdulaimogena@gmail.com>
2025-05-12 16:08:06 +0100

    Add utility functions for object parsing and sanitization
    Introduce `parseNumValues`, `removeEmptyKeys`, and `sanitizeQuery` functions to process objects. These utilities handle number parsing, removal of empty keys, and query object sanitization for improved data consistency.

---
+++ client/src/utils/object.ts
@@ -0,0 +1,50 @@
+/**
+ * Parse all number values in an object
+ * @param {Record<string, any>} obj
+ * @returns {Record<string, any>}
+ */
+export const parseNumValues = (obj: { [key: string]: any }): { [key: string]: any } => {
+    const parsedObj: { [key: string]: any } = {}
+
+    for (const key in obj) {
+        const value = obj[key]
+
+        if (typeof value === 'object' && value !== null) {
+            parsedObj[key] = parseNumValues(value)
+        } else if (!isNaN(Number(value))) {
+            parsedObj[key] = Number(value)
+        } else {
+            parsedObj[key] = value
+        }
+    }
+
+    return parsedObj
+}
+
+/**
+ * Removes empty keys in an object
+ * @param {Record<string, any>} obj
+ * @returns {Record<string, any>}
+ */
+export const removeEmptyKeys = (obj: Record<string, any>, shallow = false): Record<string, any> => {
+    for (const key in obj) {
+        if (obj[key] === null || obj[key] === undefined || obj[key] === '') {
+            delete obj[key]
+        } else if (typeof obj[key] === 'object' && !shallow) {
+            removeEmptyKeys(obj[key])
+            if (Object.keys(obj[key]).length === 0) {
+                delete obj[key]
+            }
+        }
+    }
+    return obj
+}
+
+/**
+ * Cleans a query object for empty values and parse number values
+ * @param {Record<string, any>} obj
+ * @returns {Record<string, any>}
+ */
+export const sanitizeQuery = (obj: Record<string, any>): Record<string, any> => {
+    return parseNumValues(removeEmptyKeys(obj))
+}
