567d7dfcd2b9ed4caa7bb46c4f6fedc073abe2d1
ikwemogena <abdulaimogena@gmail.com>
2025-05-15 07:51:36 +0100

    Update ESLint config and add @vue/tsconfig dependency
    Refactor ESLint config to properly organize rules and overrides for clarity and consistency. Added `@vue/tsconfig` dependency to `package.json` to support TypeScript-related settings for Vue projects.

--- ui/eslint.config.js
+++ ui/eslint.config.js
@@ -3,15 +3,16 @@ import antfu from '@antfu/eslint-config'
 export default antfu({
   formatters: true,
   vue: true,
+  rules: {
+    'unicorn/prefer-number-properties': 'off',
+    'antfu/top-level-function': 'off',
+    '@typescript-eslint/prefer-function-type': 'off',
+  },
   overrides: {
     // Force override func-style
     rules: {
       '@typescript-eslint/prefer-function-type': 'off',
-      'func-style': ['error', 'expression', { allowArrowFunction: true }], // <--- This must be in overrides for Antfu configs
+      'func-style': ['error', 'expression', { allowArrowFunction: true }],
     },
-  },
-  rules: {
-    'unicorn/prefer-number-properties': 'off',
-    '@typescript-eslint/prefer-function-type': 'off',
   },
 })
